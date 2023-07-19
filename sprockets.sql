868f008849ec6ab66bf71808f2cffa8626793c83
ruizdata <103964901+ruizdata@users.noreply.github.com>
2023-07-18 19:08:53 -0700

    Create sprockets.sql

---
+++ sprockets.sql
@@ -0,0 +1,15 @@
+*/ 
+
+Background
+
+We operate a sprocket marketplace that sells hundreds of millions of different sprockets on behalf of a large number of providers to a North American customer base. A
+customer order may include a varying set of sprockets from a variety of providers. The price of sprockets changes frequently so we update their prices and track those
+changes in a change log that is over a billion records. Due to overwhelming demand for our sprockets, our database is pushed to its limits during the business day processing millions of orders during our peak hours of operation.
+
+Task 1: Querying data
+A provider (ID: 1024) has asked for a report of their top 25 (in purchase total) customers from the past three months.
+
+Describe how you would approach fulfilling this request.
+Write the query.
+
+*/ 
